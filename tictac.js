063e3ce78967ec5d43453910dcd30459be016dc2
Boscobrand <Tony.Bosco@boscobrand.com>
2022-03-25 11:21:13 -0400

    updated pseudocode game logic

---
+++ tictac.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+    
+    const player_direction = document.querySelector('.display');
+    const boxes = Array.from(document.querySelectorAll('box'));
+    const resetButton = document.querySelector('#reset');
+
+    
+    
+//button to change box color
+
+const boxbutton = document.getElementById('box');
+
+boxbutton.addEventListener('click', function onClick(event) {
+    console.log(event.target);
+    event.target.style.backgroundColor = 'red';
+
+
+})
+    
+    
+    
+    
+    
+    
+    
+
+  });
